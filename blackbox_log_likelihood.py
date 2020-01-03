6e1aadca70f6097d246faf585f5059792622eb91
mdekauwe <mdekauwe@gmail.com>
2020-01-03 17:22:34 +1100

    refactored code

---
+++ blackbox_log_likelihood.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+"""
+Wrappers for log likelihood so that we can fit our own model...
+
+
+That's all folks.
+"""
+__author__ = "Martin De Kauwe"
+__version__ = "1.0 (03.01.2020)"
+__email__ = "mdekauwe@gmail.com"
+
+import pymc3 as pm
+import theano
+import theano.tensor as tt
+import numpy as np
+
+from partial_derivative import gradients
+
+# define a theano Op for our likelihood function
+class LogLikeWithGrad(tt.Op):
+
+    itypes = [tt.dvector] # expects a vector of parameter values when called
+    otypes = [tt.dscalar] # outputs a single scalar value (the log likelihood)
+
+    def __init__(self, loglike, data, x, sigma):
+        """
+        Initialise with various things that the function requires. Below
+        are the things that are needed in this particular example.
+
+        Parameters
+        ----------
+        loglike:
+            The log-likelihood (or whatever) function we've defined
+        data:
+            The "observed" data that our log-likelihood function takes in
+        x:
+            The dependent variable (aka 'x') that our model requires
+        sigma:
+            The noise standard deviation that out function requires.
+        """
+
+        # add inputs as class attributes
+        self.likelihood = loglike
+        self.data = data
+        self.x = x
+        self.sigma = sigma
+
+        # initialise the gradient Op (below)
+        self.logpgrad = LogLikeGrad(self.likelihood, self.data, self.x,
+                                    self.sigma)
+
+    def perform(self, node, inputs, outputs):
+        # the method that is used when calling the Op
+        theta, = inputs  # this will contain my variables
+
+        # call the log-likelihood function
+        logl = self.likelihood(theta, self.x, self.data, self.sigma)
+
+        outputs[0][0] = np.array(logl) # output the log-likelihood
+
+    def grad(self, inputs, g):
+        # the method that calculates the gradients - it actually returns the
+        # vector-Jacobian product - g[0] is a vector of parameter values
+        theta, = inputs  # our parameters
+        return [g[0]*self.logpgrad(theta)]
+
+
+class LogLikeGrad(tt.Op):
+
+    """
+    This Op will be called with a vector of values and also return a vector of
+    values - the gradients in each dimension.
+    """
+    itypes = [tt.dvector]
+    otypes = [tt.dvector]
+
+    def __init__(self, loglike, data, x, sigma):
+        """
+        Initialise with various things that the function requires. Below
+        are the things that are needed in this particular example.
+
+        Parameters
+        ----------
+        loglike:
+            The log-likelihood (or whatever) function we've defined
+        data:
+            The "observed" data that our log-likelihood function takes in
+        x:
+            The dependent variable (aka 'x') that our model requires
+        sigma:
+            The noise standard deviation that out function requires.
+        """
+
+        # add inputs as class attributes
+        self.likelihood = loglike
+        self.data = data
+        self.x = x
+        self.sigma = sigma
+
+    def perform(self, node, inputs, outputs):
+        theta, = inputs
+
+        # define version of likelihood function to pass to derivative function
+        def lnlike(values):
+            return self.likelihood(values, self.x, self.data, self.sigma)
+
+            logl = self.likelihood(theta, self.obs, self.sigma)
+        # calculate gradients
+        grads = gradients(theta, lnlike)
+
+        outputs[0][0] = grads
